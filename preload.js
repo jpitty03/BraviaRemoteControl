c2f6c5a834a8bd272fff60f12498607b8a984e86
Josh Fairbank <jpitty03@gmail.com>
2024-10-20 22:01:18 -0700

    added IRCC codes

--- preload.js
+++ preload.js
@@ -88,8 +88,72 @@ function getSupportedApiInfo(tvIp) {
     });
 }
 
+// Controller Info Method (Basic Auth Command)
+function basicAuthCommand(tvIp, service, psk, jsonStringified) {
+    return new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        xhr.open('POST', `http://${tvIp}/sony/${service}`);
+
+        if (psk) {
+            xhr.setRequestHeader('X-Auth-PSK', psk);
+        }
+
+        xhr.onload = () => {
+            if (xhr.status >= 200 && xhr.status < 300) {
+                resolve(JSON.parse(xhr.responseText));
+            } else {
+                reject(`Request failed with status ${xhr.status}`);
+            }
+        };
+
+        xhr.onerror = () => reject('Network error');
+
+        xhr.send(jsonStringified);
+    });
+}
+
+// Function to send IRCC command via SOAP request
+function sendIRCCCommand(tvIp, irccCode, psk) {
+    return new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        xhr.open('POST', `http://${tvIp}/sony/ircc`);
+
+        // Set required headers
+        xhr.setRequestHeader('Content-Type', 'text/xml; charset=UTF-8');
+        xhr.setRequestHeader('SOAPACTION', '"urn:schemas-sony-com:service:IRCC:1#X_SendIRCC"');
+        if (psk) {
+            xhr.setRequestHeader('X-Auth-PSK', psk);
+        }
+
+        xhr.onload = () => {
+            if (xhr.status >= 200 && xhr.status < 300) {
+                resolve(xhr.responseText);  // Parse SOAP response if needed
+            } else {
+                reject(`Request failed with status ${xhr.status}`);
+            }
+        };
+
+        xhr.onerror = () => reject('Network error');
+
+        // SOAP envelope for IRCC command
+        const soapEnvelope = `
+            <s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/" s:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
+                <s:Body>
+                    <u:X_SendIRCC xmlns:u="urn:schemas-sony-com:service:IRCC:1">
+                        <IRCCCode>${irccCode}</IRCCCode>
+                    </u:X_SendIRCC>
+                </s:Body>
+            </s:Envelope>
+        `;
+
+        xhr.send(soapEnvelope);
+    });
+}
+
 // Expose device discovery and command sending functions to the renderer process
 contextBridge.exposeInMainWorld('api', {
+    sendIRCCCommand: (tvIp, irccCode, psk) => sendIRCCCommand(tvIp, irccCode, psk),
+    basicAuthCommand: (tvIp, service, psk, jsonStringified) => basicAuthCommand(tvIp, service, psk, jsonStringified),
     getSupportedApiInfo: (tvIp, psk) => getSupportedApiInfo(tvIp, psk),
     sendSonyCommand: (tvIp, service, method, params, psk) => sendSonyCommand(tvIp, service, method, params, psk),
     discoverDevices: () => discoverDevices()
