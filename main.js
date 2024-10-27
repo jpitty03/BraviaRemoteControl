0d05742c759275f84dae587865461bfb38aff6e1
Josh Fairbank <jpitty03@gmail.com>
2024-10-27 12:22:24 -0700

    v1.1.2

--- main.js
+++ main.js
@@ -1,9 +1,90 @@
-const { app, BrowserWindow, ipcMain } = require('electron');
+const { app, BrowserWindow, ipcMain, Menu } = require('electron');
+const { autoUpdater } = require("electron-updater");
+const log = require('electron-log');
 const path = require('path');
 const fs = require('fs');
 
 // Path to settings file (use userData directory)
 const settingsFilePath = path.join(app.getPath('userData'), 'settings.json');
+
+// Logging
+autoUpdater.logger = log;
+autoUpdater.logger.transports.file.level = 'info';
+log.info('App starting...');
+
+//-------------------------------------------------------------------
+// Open a window that displays the version
+//
+// THIS SECTION IS NOT REQUIRED
+//
+// This isn't required for auto-updates to work, but it's easier
+// for the app to show a window than to have to click "About" to see
+// that updates are working.
+//-------------------------------------------------------------------
+let win;
+
+function sendStatusToWindow(text) {
+    log.info(text);
+    win.webContents.send('message', text);
+}
+function createDefaultWindow() {
+    win = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false
+        }
+    });
+    win.webContents.openDevTools();
+    win.on('closed', () => {
+        win = null;
+    });
+    win.loadURL(`file://${__dirname}/index.html#v${app.getVersion()}`);
+    return win;
+}
+autoUpdater.on('checking-for-update', () => {
+    sendStatusToWindow('Checking for update...');
+})
+autoUpdater.on('update-available', (info) => {
+    sendStatusToWindow('Update available.');
+})
+autoUpdater.on('update-not-available', (info) => {
+    sendStatusToWindow('Update not available.');
+})
+autoUpdater.on('error', (err) => {
+    sendStatusToWindow('Error in auto-updater. ' + err);
+})
+autoUpdater.on('download-progress', (progressObj) => {
+    let log_message = "Download speed: " + progressObj.bytesPerSecond;
+    log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
+    log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
+    sendStatusToWindow(log_message);
+})
+autoUpdater.on('update-downloaded', (info) => {
+    sendStatusToWindow('Update downloaded');
+});
+
+let template = []
+app.on('ready', function () {
+    // Create the Menu
+    const menu = Menu.buildFromTemplate(template);
+    Menu.setApplicationMenu(menu);
+
+    createWindow();
+});
+app.on('window-all-closed', () => {
+    app.quit();
+});
+
+//-------------------------------------------------------------------
+// Auto updates - Option 1 - Simplest version
+//
+// This will immediately download an update, then install when the
+// app quits.
+//-------------------------------------------------------------------
+app.on('ready', function()  {
+    console.log('checking for updates')
+    autoUpdater.checkForUpdatesAndNotify();
+  });
 
 function createWindow() {
     let mainWindow = new BrowserWindow({
@@ -16,12 +97,12 @@ function createWindow() {
             enableRemoteModule: false,
             sandbox: false
         },
-        autoHideMenuBar: true,
+        autoHideMenuBar: false,
     });
 
     mainWindow.loadFile(path.join(__dirname, 'index.html'));
 
-    // mainWindow.webContents.openDevTools();
+    mainWindow.webContents.openDevTools();
 
     mainWindow.on('closed', () => {
         mainWindow = null;
@@ -61,7 +142,7 @@ ipcMain.on('save-settings', (event, settings) => {
     saveSettings(settings);
 });
 
-app.whenReady().then(createWindow);
+// app.whenReady().then(createWindow);
 
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
